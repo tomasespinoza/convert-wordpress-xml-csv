f1a98b89e214173095aee1c7f6584967d2af5389
tomasespinoza <31084595+tomasespinoza@users.noreply.github.com>
2024-09-15 17:30:21 -0700

    Update convert-wp-xml.py

--- convert-wp-xml.py
+++ convert-wp-xml.py
@@ -1,16 +1,14 @@
-# erase lines between the opening <rss> bracket and the first <item> from source wp xml export:
+# prep xml file: erase lines between the opening <rss> bracket and the first <item> from source:
 # i.e channel, description, pubDate, language, and any beginning with "wp:", 
 
-# import feedparser
 import xml.etree.ElementTree as Xet
 import pandas as pd
 
-# data = feedparser.parse('/content/surgicaleducationlearnsurgery.WordPress.2024-07-10.xml')
 cols = ["title", "link"]
 rows = []
 
 # Parsing the XML file
-xmlparse = Xet.parse('/content/surged7-12.xml')
+xmlparse = Xet.parse('PATH_TO_XML_FILE')
 root = xmlparse.getroot()
 
 for item in root:
